d3e8c1987ba4293cea0d33d696c51c2af9f6ad65
David <doczi.david.daci@gmail.com>
2016-06-27 11:55:59 +0200

    test.py plus cases added

--- test.py
+++ test.py
@@ -28,6 +28,13 @@ class TestCount_letter_in_string(unittest.TestCase):
         self.assertEqual(count_letter_in_string('Hello', (0,1,2,3,4,5,6,7,8,9)), 0)
     def test_not_letter_not_one_letter(self):
         self.assertEqual(count_letter_in_string('Hello', 'aa'), 0)
+    def test_all_letter_same(self):
+        self.assertEqual(count_letter_in_string('aaaaaa', 'a'), 6)
+    def test_all_same_upper_letter(self):
+        self.assertEqual(count_letter_in_string('AAAAAA', 'a'), 0)
+    def test_all_letter_same_upper_and_lower(self):
+        self.assertEqual(count_letter_in_string('AaAaAa', 'a'), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
