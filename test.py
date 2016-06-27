8f542bf0c4bacc4e3b18fe819551360f9d3f34e9
David <doczi.david.daci@gmail.com>
2016-06-27 10:19:58 +0200

    third.py  added

---
+++ test.py
@@ -0,0 +1,33 @@
+from third import count_letter_in_string
+import unittest
+
+class TestCount_letter_in_string(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(count_letter_in_string('', 'a'), 0)
+    def test_not_string_int(self):
+        self.assertEqual(count_letter_in_string(1, 'a'), 0)
+    def test_not_string_float(self):
+        self.assertEqual(count_letter_in_string(1.1, 'a'), 0)
+    def test_not_string_list(self):
+        self.assertEqual(count_letter_in_string([0,1,2,3,4,5,6,7,8,9], 'a'), 0)
+    def test_not_string_dict(self):
+        self.assertEqual(count_letter_in_string({0,1,2,3,4,5,6,7,8,9}, 'a'), 0)
+    def test_not_string_tuple(self):
+        self.assertEqual(count_letter_in_string((0,1,2,3,4,5,6,7,8,9), 'a'), 0)
+    def test_empty_letter(self):
+        self.assertEqual(count_letter_in_string('Hello', ''), 0)
+    def test_not_letter_int(self):
+        self.assertEqual(count_letter_in_string('Hello', 1), 0)
+    def test_not_letter_float(self):
+        self.assertEqual(count_letter_in_string('a', 1.1), 0)
+    def test_not_letter_list(self):
+        self.assertEqual(count_letter_in_string('Hello', [0,1,2,3,4,5,6,7,8,9]), 0)
+    def test_not_letter_dict(self):
+        self.assertEqual(count_letter_in_string('Hello', {0,1,2,3,4,5,6,7,8,9}), 0)
+    def test_not_letter_tuple(self):
+        self.assertEqual(count_letter_in_string('Hello', (0,1,2,3,4,5,6,7,8,9)), 0)
+    def test_not_letter_not_one_letter(self):
+        self.assertEqual(count_letter_in_string('Hello', 'aa'), 0)
+
+if __name__ == '__main__':
+    unittest.main()
