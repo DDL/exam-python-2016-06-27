f13b6f30500079b19eeb4793084cd65c392391ce
David <doczi.david.daci@gmail.com>
2016-06-27 09:29:35 +0200

    first.py added

--- first.py
+++ first.py
@@ -3,3 +3,13 @@
 # It should raise an error if the parameter is not a list
 # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
+raw_list = [1, 2, 3, 4, 5]
+
+def copy_every_second_element_from_list(raw_list):
+    if type(raw_list) != list:
+        raise ValueError('Excepted a list')
+    even_indexed_items = []
+    even_indexed_items = raw_list[1::2]
+    return even_indexed_items
+
+print(copy_every_second_element_from_list(raw_list))
