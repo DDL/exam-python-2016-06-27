c7b6788e1049e308ee7e396f80213289f3f612eb
David <doczi.david.daci@gmail.com>
2016-06-27 09:55:10 +0200

    second.py and second.txt added

--- second.py
+++ second.py
@@ -4,3 +4,15 @@
 # it should write "appleappleapple" to the file "tree.txt".
 # the function should not raise an error on any output problem, for example
 # denied permission
+file_name = 'second.txt'
+string = 'apple'
+
+def write_string_3times_to_file(file_name, string):
+    try:
+        file = open(file_name, 'w')
+        file.write(string * 3)
+        file.close()
+    except:
+        pass
+
+write_string_3times_to_file(file_name, string)
